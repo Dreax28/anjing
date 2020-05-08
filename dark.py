dc87abe85a1161dd09d54ed8420d9327b078c22d
Dreax28 <64790995+Dreax28@users.noreply.github.com>
2020-05-08 15:58:08 +0700

    Add files via upload

---
+++ dark.py
@@ -0,0 +1,23 @@
+import os
+import sys
+os.system('clear')
+os.system('figlet Hack Fb')
+print "==============================="
+print " KONTOL MEMEK ANJING BANGSAT"
+print "     Author : Dreax"
+print "  Youtube : DREAX GAMING"
+print "  Github : https://github.com/Dreax28"
+print "==============================="
+print 
+print "[+] Menu Pilihan [+]"
+print "[1] Dark Fbv1.7"
+print "[2] Dark Fbv1.8"
+print "[3] Dark Fb Premium"
+print "[4] Dark Fbv2.0"
+print "[5] Install Bahan"
+pilih = raw_input(' [?] pilih ')
+if pilih == "1":
+           os.system('git clone https://github.com/Dreax28/dark-fb
+           print "[+] penginstalan selesai"
+           print "[+] selanjutnya ketik $ cd dark"
+           print "[+] lalu ketik $ python2 darkfb.py"
